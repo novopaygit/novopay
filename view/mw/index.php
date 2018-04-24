5401f3ac3ec9f84d0e9b3286289783e35a7a0d4a
ChoiInSeok <cis@novowave.co.kr>
2018-04-24 17:36:23 +0900

    메인화면 링크추가

--- view/mw/index.php
+++ view/mw/index.php
@@ -19,30 +19,31 @@
 						</div>
 					</header>
 
-					<!-- <section class="main-cont-wrap">
+					 <section class="main-cont-wrap">
 					    <ul class="main-cont-box">
 						    <li>
-							    <a href="#none" class="bg-green co-gray33">
-								    <h4>거래내역조</h4>
-									<p>전체거래내역,회 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
+							    <a href="/mw/transaction/transactionlist.php" class="bg-green co-gray33">
+								    <h4>거래내역조회</h4>
+									<p>전체거래내역, 취소거래내역 등을 확인 하실 수있습니다. </p>
 									<span class="s-btn-more">more</span>
 								</a>
 							</li>
 						    <li>
-							    <a href="#none" class="bg-red co-white">
-								    <h4>정산내역조</h4>
-									<p>전체거래내역, 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
+							    <a href="/mw/transaction/calculatelist.php" class="bg-red co-white">
+								    <h4>정산내역조회</h4>
+									<p>정산내역, 정산상세내역 등을 확인하실 수 있습니다.</p>
 									<span class="s-btn-more">more</span>
 								</a>
 							</li>
 						     <li>
-							    <a href="#none" class="bg-yellow co-gray33">
-								    <h4>직불결제</h4>
-									<p>전체거래내역, 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
+							    <a href="/mw/transaction/calculatelist.php" class="bg-yellow co-gray33">
+								    <h4>정산관리</h4>
+									<p>정산처리, 정산취소 , 정산조회 등을 등록 관리하실 수 있습니다.</p>
 									<span class="s-btn-more">more</span>
 								</a>
 							</li> 
 						</ul>
+						<!--
 					     <ul class="main-cont-box mgt5">
 						    <li>
 							    <a href="#none" class="bg-sky co-white">
@@ -65,8 +66,8 @@
 									<span class="s-btn-more">more</span>
 								</a>
 							</li>
-						</ul> 
-					</section>		 -->	
+						</ul> -->
+					</section>		 	
 				</div><!-- /inner -->
 			</div><!-- //main -->
 
