8f93b88df42cb6b5070fb9767349467316ee76c9
ChoiInSeok <cis@novowave.co.kr>
2018-04-24 16:19:53 +0900

    메인화면 불필요항목제거

--- view/mw/index.php
+++ view/mw/index.php
@@ -18,31 +18,32 @@
 							<p>간편함 그 이상의 결제 노보페이!<br />노보페이 가맹점 관리시스템에 오신 여러분을 환영합니다.</p>
 						</div>
 					</header>
-					<section class="main-cont-wrap">
+
+					<!-- <section class="main-cont-wrap">
 					    <ul class="main-cont-box">
 						    <li>
 							    <a href="#none" class="bg-green co-gray33">
-								    <h4>통합내역조회</h4>
-									<p>전체거래내역, 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
+								    <h4>거래내역조</h4>
+									<p>전체거래내역,회 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
 									<span class="s-btn-more">more</span>
 								</a>
 							</li>
 						    <li>
 							    <a href="#none" class="bg-red co-white">
-								    <h4>배송통계</h4>
+								    <h4>정산내역조</h4>
 									<p>전체거래내역, 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
 									<span class="s-btn-more">more</span>
 								</a>
 							</li>
-						    <li>
+						     <li>
 							    <a href="#none" class="bg-yellow co-gray33">
 								    <h4>직불결제</h4>
 									<p>전체거래내역, 전체 거절내역, 미배송 거래내역, 구매취소내역 등을 확인하실 수 있습니다.</p>
 									<span class="s-btn-more">more</span>
 								</a>
-							</li>
+							</li> 
 						</ul>
-					    <ul class="main-cont-box mgt5">
+					     <ul class="main-cont-box mgt5">
 						    <li>
 							    <a href="#none" class="bg-sky co-white">
 								    <h4>통합내역조회</h4>
@@ -64,8 +65,8 @@
 									<span class="s-btn-more">more</span>
 								</a>
 							</li>
-						</ul>
-					</section>			
+						</ul> 
+					</section>		 -->	
 				</div><!-- /inner -->
 			</div><!-- //main -->
 
